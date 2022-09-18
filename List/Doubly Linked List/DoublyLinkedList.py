d731682e87d86e755a9b510aaff0b29ad52fc593
Lucas Xavier <lucaseoxavier@gmail.com>
2022-09-18 20:25:14 -0300

    constructors defined

--- List/Doubly Linked List/DoublyLinkedList.py
+++ List/Doubly Linked List/DoublyLinkedList.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+
 class ListADT(ABC):
 
     @abstractmethod
@@ -50,4 +51,56 @@ class ListADT(ABC):
     @abstractmethod
     def length(self):
         """Returns length of the list"""
+        pass
+
+
+class DoublyLinkedList(ListADT):
+
+    class _Node:
+        def __init__(self, elem=None, next=None, prev=None):
+            self._elem = elem
+            self._next = next
+            self._prev = prev
+
+        def __str__(self):
+            if self._next is None:
+                return self._elem.__str__()
+            else:
+                return self._elem.__str__() + "<->"
+
+    def __init__(self):
+        self._header = self._Node()
+        self._trailer = self._Node()
+        self._header._next = self._trailer
+        self._trailer._prev = self._header
+        self.length = 0
+
+    def insert(self, index, elem):
+        pass
+
+    def remove(self, elem):
+        pass
+
+    def remove_all(self, elem):
+        pass
+
+    def remove_at(self, index):
+        pass
+
+    def count(self, elem):
+        pass
+
+    def clear(self):
+        pass
+
+    def index(self, elem):
+        pass
+
+    def append(self, elem):
+        pass
+
+    def replace(self, index, elem):
+        pass
+
+    def length(self):
         pass
