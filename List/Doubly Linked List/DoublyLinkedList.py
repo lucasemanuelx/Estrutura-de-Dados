8ab54fd437c331ed3749ad58a084edb69a6c07d5
Lucas Xavier <lucaseoxavier@gmail.com>
2022-09-18 19:52:43 -0300

    list ADT

---
+++ List/Doubly Linked List/DoublyLinkedList.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+class ListADT(ABC):
+
+    @abstractmethod
+    def insert(self, index, elem):
+        """Inserts <elem> in <index>"""
+        pass
+
+    @abstractmethod
+    def remove(self, elem):
+        """Removes first occurence of <elem>"""
+        pass
+
+    @abstractmethod
+    def remove_all(self, elem):
+        """Removes all occurences of <elem>"""
+        pass
+
+    @abstractmethod
+    def remove_at(self, index):
+        """Removes node at <index>"""
+        pass
+
+    @abstractmethod
+    def count(self, elem):
+        """Counts <elem> in list"""
+        pass
+
+    @abstractmethod
+    def clear(self):
+        """Clears lists"""
+        pass
+
+    @abstractmethod
+    def index(self, elem):
+        """Returns index of <elem>"""
+        pass
+
+    @abstractmethod
+    def append(self, elem):
+        """Appends <elem> at the end of the list """
+        pass
+
+    @abstractmethod
+    def replace(self, index, elem):
+        """Replaces node at <index> with <elem>"""
+        pass
+
+    @abstractmethod
+    def length(self):
+        """Returns length of the list"""
+        pass
